c7318c31faca868f7931effbc44cc1f158f64d8e
Mridupawan Bordoloi <mridupawan503@gmail.com>
2025-04-07 17:15:19 +0530

    feat: add multer middleware for image upload functionality

---
+++ backend/middlewares/multer.middleware.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// file filter
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpeg, .png, .jpeg formats are allowed"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+module.exports = upload;
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { protected } = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/multer.middleware");
 
 const router = express.Router();
 const {
@@ -7,11 +8,16 @@ const {
   loginUser,
   getUserProfile,
   updateUserProfile,
+  uploadImage,
 } = require("../controllers/auth.controllers");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/profile").get(protected, getUserProfile);
 router.route("/update-profile").put(protected, updateUserProfile);
+
+router
+  .route("/upload-image")
+  .post(protected, upload.single("profile"), uploadImage);
 
 module.exports = router;
