5360db1b4dad95ed81913a5d4ae2e5d3293f900e
Mridupawan Bordoloi <mridupawan503@gmail.com>
2025-04-07 19:17:43 +0530

    feat: add user routes for fetching and managing user data

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { protected, adminProtected } = require("../middlewares/auth.middleware");
+const {
+  getAllUsers,
+  getUserById,
+  deleteUserById,
+} = require("../controllers/user.controllers.js");
+
+const router = express.Router();
+
+// user
+router.route("/:id").get(protected, getUserById);
+
+// admin
+router.route("/").get(protected, adminProtected, getAllUsers);
+router.route("/:id").delete(protected, adminProtected, deleteUserById);
+
+module.exports = router;
